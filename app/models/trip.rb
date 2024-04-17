679c2c6d05ebcdd601371e449c53abf610e5ecce
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-04-17 14:08:50 -0700

    seed dump

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,4 @@
 class Trip < ApplicationRecord
+  belongs_to :user
+  has_many :places
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!([
+  {name: "Bob", email: "bob@test.com", password_digest: "password", image_url: "https://i.redd.it/tmw8hpbcll791.jpg"},
+  {name: "Rick", email: "rick@test.com", password_digest: "password", image_url: "https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png"}
+])
